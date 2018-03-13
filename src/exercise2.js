4658b4f5d9bab2f171eaea9dc59ca048cb2277f3
Antonin Reillon <antonin.reillon@ynov.com>
2018-03-13 10:43:03 +0100

    début exercice 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,5 +1,14 @@
 var mapCustomer = function(customer) {
   // Your future job begins here ...
+
+  var totalMoneySpent;
+  var output = customer;
+
+  for (let index = 0; index < customer.length; index++) {
+
+  }
+
+  return output
 }
 
 module.exports = {
