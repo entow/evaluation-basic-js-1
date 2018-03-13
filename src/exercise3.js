df90f2e0a99d03008928d07e0ac34c32ac242c4e
Antonin Reillon <antonin.reillon@ynov.com>
2018-03-13 10:46:58 +0100

    début exercice 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,5 +1,24 @@
 var applyMap = function() {
   // Your future job begins here ...
+  var applyMap = function(inputValues, inputMaps) {
+  if (inputMaps === undefined) {
+    return output = []
+  }
+
+    else {
+    for (let index = 0; index < inputMaps.length; index++) {
+
+      for (let itereur = 0; itereur < inputValues.length; itereur++) {
+
+      }
+
+      output.push();
+    }
+
+    return output;
+
+    }
+  }
 }
 
 module.exports = {
